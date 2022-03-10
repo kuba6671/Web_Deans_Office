99766610fe4218e1defbfff2f3e78276b365a84f
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2022-03-10 20:50:50 +0100

    Added application template

---
+++ src/main/java/office/deans/web/DeansOffice/DeansOfficeApplication.java
@@ -0,0 +1,13 @@
+package office.deans.web.DeansOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeansOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeansOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/office/deans/web/DeansOffice/DeansOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package office.deans.web.DeansOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeansOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
